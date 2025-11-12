bb451884021139ff12a637378f311b6d29ac4f24
Trana25 <trana25@iastate.edu>
2025-11-12 16:53:50 -0600

    removed old productdev main, working on new one.

--- src/coms3620/fashion/FashionManager.java
+++ src/coms3620/fashion/FashionManager.java
@@ -4,7 +4,6 @@ import coms3620.fashion.menus.Menu;
 import coms3620.fashion.menus.marketing_and_sales.MarketingAndSales;
 import coms3620.fashion.menus.logistics.Logistics;
 import coms3620.fashion.menus.human_resources.HumanResources;
-import coms3620.fashion.menus.product_dev.ProductDevelopment;
 
 
 
@@ -15,11 +14,9 @@ public class FashionManager extends Menu {
         // Define options
         MarketingAndSales marketing_and_sales = new MarketingAndSales();
         Logistics logistics = new Logistics();
-    ProductDevelopment productDev = new ProductDevelopment();
         // Add options
         addOption(marketing_and_sales);
         addOption(logistics);
     addOption(new HumanResources());
-    addOption(productDev);
     }
 }
